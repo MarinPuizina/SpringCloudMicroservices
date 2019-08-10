eb58a9cde658f14f73bd0fde5464f4e0e25f12e1
MarinPuizina <marinpuizina@hotmail.com>
2019-08-10 23:03:49 +0200

    Added login API.

--- microservices-api-users/src/main/java/com/app/api/users/service/UsersService.java
+++ microservices-api-users/src/main/java/com/app/api/users/service/UsersService.java
@@ -1,8 +1,9 @@
 package com.app.api.users.service;
 
 import com.app.api.users.shared.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
 
     UserDto createUser(UserDto userDetails);
 
