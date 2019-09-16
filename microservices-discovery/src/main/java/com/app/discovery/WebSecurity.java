2081eae2934c2ac14c25104b906f6b1dbf8c4608
MarinPuizina <marinpuizina@hotmail.com>
2019-09-16 21:03:54 +0200

    Add Spring Security

---
+++ microservices-discovery/src/main/java/com/app/discovery/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.app.discovery;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+    }
+
+}
